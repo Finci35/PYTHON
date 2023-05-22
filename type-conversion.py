c2adcfd137cfb6a946f7bd3aaabb3c38d794c4ed
Finci35 <2349fatih@gmail.com>
2023-05-22 13:10:56 +0200

    22.05-13.10

---
+++ type-conversion.py
@@ -0,0 +1,63 @@
+# Veri tipi dönüştürme
+
+"""
+x = input("1.sayı :")
+y = input("2.sayı :")
+
+print(type(x))
+print(type(x))
+
+toplam = int(x) + int(y)
+
+print(toplam)
+"""
+
+x = 5             # int
+y = 2.5           # float
+name = "Çınar"    # str
+isOnline = True   # bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# -------------------------------------------
+
+# Type Conversion
+
+# int to float
+
+# x = float(x)
+# print(type(x))
+# print(x)
+
+#------------------------------------------------
+
+# float to int
+
+# y = int(y)
+# print(type(y))
+# print(y)
+
+# ----------------------------------------------------
+
+# result = str(x) + str(y)
+# print(result)
+# print(type(result))
+
+#------------------------------------------------------
+
+# bool to str
+
+# isOnline = str(isOnline)
+# print(isOnline)
+# print(type(isOnline))
+
+# -------------------------------------------------------------
+
+# bool to int
+
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
---
+++ variables.py
@@ -0,0 +1,22 @@
+musteriAdi = "Fatih"
+musteriSoyad = "Perle"
+musteriAdSoyad = musteriAdi + " " + musteriSoyad
+musteriCinsiyet = True # Erkek
+musteriTCKimlik = "12345678901"
+musteriDogumYili = 1983
+musteriAdresi = "İzmir"
+musteriYasi = 2023 - musteriDogumYili
+
+print(musteriYasi)
+
+
+
+# Siparişleri Topla
+
+siparis1 = 110
+siparis2 = 1100.5
+siparis3 = 356.95
+
+Toplam = siparis1 + siparis2 + siparis3
+
+print("Toplam: ", Toplam)
