7c69a3215632e13340fb19026331da1ca97ad7b6
Finci35 <2349fatih@gmail.com>
2023-05-24 09:41:42 +0200

    24.05-09.40

---
+++ stringMethods.py
@@ -0,0 +1,10 @@
+message = "  Hello There. My name is Fatih."
+
+message = message.upper() # message değişkeni içindeki tüm karakterleri büyük harf yapar.
+message = message.lower() # tüm karakterleri küçük harf yapar.
+message = message.title() # her kelimenin baş harfini büyük yapar.
+message = message.capitalize() # verilen string ifadenin sadece ilk harfini büyük yapar.
+message = message.strip() # verilen string ifadenin başında boşluk varsa onu yok eder.
+
+
+print(message)
