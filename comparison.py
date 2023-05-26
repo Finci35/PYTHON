4bdeed7c861013dc1865ea60f0f4c2c294e009f1
Finci35 <2349fatih@gmail.com>
2023-05-26 11:08:45 +0200

    22.05-11.05

---
+++ comparison.py
@@ -0,0 +1,21 @@
+# ******************* KARŞILAŞTIRMA OPERATÖRLERİ ************************
+
+a, b, c, d = 5, 5, 10, 4
+
+password = '1234'
+username = 'sadikturan'
+
+result = (a == b) # True
+result = (a == c) # False
+result = ('sdktrn' == username) # False
+result = ('sadikturan' == username) # True
+result = (a != b) # False
+result = (a != c) # True
+result = (a > c) # False
+result = (a < b) # True
+result = (a >= b) # True    
+result = (True == 1) # True
+result = (False == 0) # True
+result = (True  + False + 40) # 41
+
+print(result)
