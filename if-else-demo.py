ccea4091bc7e41d4190a7cde96aa1d2e5a1f347c
Finci35 <2349fatih@gmail.com>
2023-06-01 12:08:08 +0200

    01.06-12.05

--- if-else-demo.py
+++ if-else-demo.py
@@ -24,26 +24,26 @@
 # 70 - 84  => 4
 # 85 - 100 => 5
 
-yazili1 = int(input('Birinci yazılı noutunuz: '))
-yazili2 = int(input('İkinci yazılı noutunuz: '))
-sozlu = int(input('Sözlü noutunuz: '))
+# yazili1 = int(input('Birinci yazılı noutunuz: '))
+# yazili2 = int(input('İkinci yazılı noutunuz: '))
+# sozlu = int(input('Sözlü noutunuz: '))
 
-ortalama = (yazili1 + yazili2 + sozlu) / 3
+# ortalama = (yazili1 + yazili2 + sozlu) / 3
 
-if 0 <= ortalama <= 24:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 0')
-elif 25 <= ortalama <= 44:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 1')
-elif 45 <= ortalama <= 54:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 2')
-elif 55 <= ortalama <= 69:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 3')
-elif 70 <= ortalama <= 84:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 4')
-elif 85 <= ortalama <= 100:
-    print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 5')
-else :
-    print('Geçersiz bir veri girdiniz.')
+# if 0 <= ortalama <= 24:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 0')
+# elif 25 <= ortalama <= 44:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 1')
+# elif 45 <= ortalama <= 54:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 2')
+# elif 55 <= ortalama <= 69:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 3')
+# elif 70 <= ortalama <= 84:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 4')
+# elif 85 <= ortalama <= 100:
+#     print(f'Ortalamanız: {ortalama:2.4} --- Notunuz: 5')
+# else :
+#     print('Geçersiz bir veri girdiniz.')
     
     
 # 3- Trafiğe çıkış tarihi alınan bir aracın servis zamanını aşağıdaki bilgilere göre hesaplayınız.
@@ -54,6 +54,26 @@ else :
 # ** Süre hesabını alınan gün, ay, yıl bilgisine göre gün bazlı yapınız.
 # *** datetime modülünü kullanılmalı.
 
+import datetime
+
+tarih = input('Aracınız hangi tarihte trafiğe çıktı (2019/8/9)')
+tarih = tarih.split('/')
+
+trafigeCikis = datetime.datetime(int(tarih[0]), int(tarih[1]), int(tarih[2]))
+
+simdi = datetime.datetime.now()
+fark = simdi - trafigeCikis # Çıktı: "600 days" şeklindedir. 
+                            #Çıkarma işlemi yapabilmek için days ifadesinin kaldırılması gerekir.
+days = fark.days # Bu işlem "days ifadesini kaldırır."
+
+if days <= 365:
+    print("1. servis aralığı")
+elif days > 365 and days <= 365*2 :
+    print("2. servis aralığı")
+elif days > 365*2 and days <= 365*3 :
+    print("3. servis aralığı")
+else :
+    print("Hatalı süre")
 #
 
 #
